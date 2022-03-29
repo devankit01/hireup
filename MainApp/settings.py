c71f803235a838343bb94ec9dcfd587cde8c1649
ankit.gupta20 <ankit.gupta@appventurez.com>
2022-03-29 22:33:28 +0530

    Prepup Admin Panel

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -111,8 +111,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR , 'static'),
 ]
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
 
 # Email Setup
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
