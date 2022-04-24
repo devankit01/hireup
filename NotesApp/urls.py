0fdc59c667d0a2d956a6ab50888a6069d9e77fca
Adil <adil.khan@appventurez.com>
2022-04-24 13:11:03 +0530

    just have to check tag part

--- NotesApp/urls.py
+++ NotesApp/urls.py
@@ -5,11 +5,18 @@ from .views import *
 urlpatterns = [
     path('', prepIndex, name='prepIndex'),
     path('interview-prep', interviewIndex, name='interviewIndex'),
+    path('interview-prep/admin/', interviewIndexAdmin, name='interviewIndexAdmin'),
     path('admin/', PrepupAdmin, name="PrepupAdmin"),
     path('open-file/<int:id>', OpenFIle, name="OpenFIle"),
     path('prep-update/<int:id>/<str:status>', prepUpdate, name="prepUpdate"),
     path('prep-edit/<str:id>', prepEdit, name="prepEdit"),
     path('prep-edit/', prepEdit, name="prepEdit"),
+
+    path('interview-prep-update/<int:id>/<str:status>',
+         interviewPrepUpdate, name="interviewPrepUpdate"),
+    path('interview-prep-edit/<str:id>',
+         interviewPrepEdit, name="interviewPrepEdit"),
+    path('interview-prep-edit/', interviewPrepEdit, name="interviewPrepEdit"),
 
 
 ]
