30e0d389437271f59686477ad2df588d1e3a707d
Adil <adil.khan@appventurez.com>
2022-04-30 11:24:35 +0530

    resolved user profile update

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -70,7 +70,7 @@ ASGI_APPLICATION = 'MainApp.asgi.application'
 
 AWS_S3_ENDPOINT_URL = 'https://s3.amazonaws.com'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = 'ap-south-1'
+AWS_S3_REGION_NAME = 'us-east-1'
 AWS_ACCESS_KEY_ID = 'AKIAXLSZRNQVPA26ZV4D'
 AWS_SECRET_ACCESS_KEY = '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'
 AWS_STORAGE_BUCKET_NAME = 'hireup-project'
