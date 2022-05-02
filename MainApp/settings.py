93ada850ffade250a22bf584be6026e72fa33f29
Adil <adil.khan@appventurez.com>
2022-05-02 13:56:26 +0530

    now resume is visible

--- MainApp/settings.py
+++ MainApp/settings.py
@@ -68,13 +68,14 @@ WSGI_APPLICATION = 'MainApp.wsgi.application'
 ASGI_APPLICATION = 'MainApp.asgi.application'
 
 
-AWS_S3_ENDPOINT_URL = 'https://s3.amazonaws.com'
+# s3 settings
+
+AWS_S3_REGION_NAME = 'ap-south-1'
+AWS_STORAGE_BUCKET_NAME = 'shift-agreements'
+AWS_QUERYSTRING_AUTH = False
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_ACCESS_KEY_ID = 'AKIAXLSZRNQVPA26ZV4D'
-AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'hireup-project'
-AWS_DEFAULT_ACL = None
+AWS_ACCESS_KEY_ID = 'AKIASGXAF2R2OVE5FVGP'
+AWS_SECRET_ACCESS_KEY = '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'
 
 # DATABASES = {
 #     "default": {
