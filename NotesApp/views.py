1d8aacdb4a45eb15f33bcd0263a5b3d60f95de16
Adil <adil.khan@appventurez.com>
2022-05-09 11:11:01 +0530

    added url in prepinterview

--- NotesApp/views.py
+++ NotesApp/views.py
@@ -28,7 +28,9 @@ def prepIndex(request):
 
 def interviewIndex(request):
     Interviews = InterviewPrep.objects.filter(IsApproved=True)
-    print(Interviews)
+    for inter in Interviews:
+        inter.file = "https://shift-agreements.s3.ap-south-1.amazonaws.com/" + \
+            str(inter.file)
     logger.info('Interview  Page Loaded')
     return render(request, 'prepup/interviewPrep.html', {'data': Interviews})
 
